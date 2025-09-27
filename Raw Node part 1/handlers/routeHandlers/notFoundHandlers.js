cc8713d85e0fd61d94697196934871bc742e315e
rakibul1 <rakib@grameendhs.com>
2025-09-27 11:26:42 +0600

    part 2 started

--- Raw Node part 1/handlers/routeHandlers/notFoundHandlers.js
+++
@@ -1,9 +0,0 @@
-const handler = {}
-
-
-handler.notFoundHandlers = (requestProperties, callback) => {
-    callback(404, {
-        message: 'Not Found',
-    })
-}
-module.exports = handler
--- Raw Node part 1/routes.js
+++
@@ -1,8 +0,0 @@
-
-const {sampleHandlers}=require('./handlers/routeHandlers/samplehandlers');
-
-const routes={
-"sample":sampleHandlers
-}
-
-module.exports=routes;
